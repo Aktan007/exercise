52b0b9c48d7e34fcbf27e0eb4b51e00f5acce8d0
Aktan Ibr. <aktan.ibraev05@mail.ru>
2023-10-19 23:37:30 +0300

    new test

--- __tests__/all.test.js
+++ __tests__/all.test.js
@@ -1,16 +1,16 @@
-import { test, expect } from '@jest/globals';
-import { getSymbol, removeDigit, isSquare, numberLength, flipOver } from '../src/functions.js';
+import { test, expect } from "@jest/globals";
+import { getSymbol, removeDigit, flipOver } from "../src/functions.js";
 
-test('getSymbol', () => {
-  expect(getSymbol('The members', 4)).toEqual('m');
-  expect(getSymbol('type can in', 1)).toEqual('y');
-  expect(getSymbol('an error', 20)).toEqual('');
-  expect(getSymbol('qwerty', 0)).toEqual('q');
-  expect(getSymbol('QwErTy', 0)).toEqual('Q');
-  expect(getSymbol('', 0)).toEqual('');
+test("getSymbol", () => {
+  expect(getSymbol("The members", 4)).toEqual("m");
+  expect(getSymbol("type can in", 1)).toEqual("y");
+  expect(getSymbol("an error", 20)).toEqual("");
+  expect(getSymbol("qwerty", 0)).toEqual("q");
+  expect(getSymbol("QwErTy", 0)).toEqual("Q");
+  expect(getSymbol("", 0)).toEqual("");
 });
 
-test('removeDigit', () => {
+test("removeDigit", () => {
   expect(removeDigit(208)).toBe(28);
   expect(removeDigit(109)).toBe(19);
   expect(removeDigit(940)).toBe(90);
@@ -18,7 +18,7 @@ test('removeDigit', () => {
   expect(removeDigit(567)).toBe(57);
 });
 
-test('isSquare', () => {
+test("isSquare", () => {
   expect(isSquare(2, 4)).toBeTruthy();
   expect(isSquare(81, 9)).toBeTruthy();
   expect(isSquare(25, 125)).toBeFalsy();
@@ -26,7 +26,7 @@ test('isSquare', () => {
   expect(isSquare(16, -4)).toBeTruthy();
 });
 
-test('numberLength', () => {
+test("numberLength", () => {
   expect(numberLength(123)).toBe(3);
   expect(numberLength(3)).toBe(1);
   expect(numberLength(19283)).toBe(5);
@@ -34,10 +34,10 @@ test('numberLength', () => {
   expect(numberLength(1)).toBe(1);
 });
 
-test('flipOver', () => {
-  expect(flipOver('hello')).toEqual('olleh');
-  expect(flipOver('swap')).toEqual('paws');
-  expect(flipOver('John Doe')).toEqual('eoD nhoJ');
-  expect(flipOver('qWeRt')).toEqual('tReWq');
-  expect(flipOver('1029384756')).toEqual('6574839201');
+test("flipOver", () => {
+  expect(flipOver("hello")).toEqual("olleh");
+  expect(flipOver("swap")).toEqual("paws");
+  expect(flipOver("John Doe")).toEqual("eoD nhoJ");
+  expect(flipOver("qWeRt")).toEqual("tReWq");
+  expect(flipOver("1029384756")).toEqual("6574839201");
 });
--- src/functions.js
+++ src/functions.js
@@ -9,9 +9,16 @@ console.log(getSymbol("The members", 4));
 console.log(getSymbol("type can in", 1));
 console.log(getSymbol("an error", 20));
 
-const removeDigit = () => {
-  return;
+const removeDigit = (array) => {
+  const gtr = String(array);
+  const btrDigit = gtr[0];
+  const ztrDigit = gtr[2];
+  return parseInt(btrDigit + ztrDigit);
 };
+
+console.log(removeDigit(208));
+console.log(removeDigit(109));
+console.log(removeDigit(940));
 
 const isSquare = () => {
   return;
@@ -21,8 +28,12 @@ const numberLength = () => {
   return;
 };
 
-const flipOver = () => {
-  return;
+const flipOver = (str) => {
+  return str.split("").reverse().join("");
 };
 
-export { getSymbol };
+console.log(flipOver("hello"));
+console.log(flipOver("swap"));
+console.log(flipOver("John Doe"));
+
+export { getSymbol, removeDigit, flipOver };
